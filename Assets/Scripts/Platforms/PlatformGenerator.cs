925d6da89f638b6e4bc03cc11f548d2bc02d4536
Trysac <jmgomezsac@gmail.com>
2021-04-23 22:03:33 -0500

    Assets Cleanup (free polygon), Platform Spawner Fixed

--- Assets/Scripts/Platforms/PlatformGenerator.cs
+++ Assets/Scripts/Platforms/PlatformGenerator.cs
@@ -7,6 +7,7 @@ public class PlatformGenerator : MonoBehaviour
     [Header("Genereal Parameters")]
     [SerializeField] float platformTravelSpeed = 2;
     [SerializeField] int platformLimitCount = 4;
+    [SerializeField] Transform platformsContainer;
 
     [Header("Prefabs")]
     [SerializeField] Platform[] basicPlatform;
@@ -14,22 +15,40 @@ public class PlatformGenerator : MonoBehaviour
     [SerializeField] GameObject[] HardPlatform;
     [SerializeField] GameObject[] interceptionPlatform;
 
-    int platformsInWorld;
+    //int platformsInWorld;
+    private void Start()
+    {
+        StartCoroutine(SpawnPlatforms());
+    }
 
     void Update()
     {
         if (Game_Manager.IsGameActive) 
         {
-            platformsInWorld = FindObjectsOfType<Platform>().Length;
-            SpawnPlatform();
+            //platformsInWorld = FindObjectsOfType<Platform>().Length;
+            //SpawnPlatform();
+        }
+        else 
+        {
+            StopCoroutine(SpawnPlatforms());
         }
     }
 
     private void SpawnPlatform() 
     {
-        if (platformsInWorld >= platformLimitCount) {return;}
+        //if (platformsInWorld >= platformLimitCount) {return;}
         Platform platform = Instantiate(basicPlatform[0], new Vector3(0,0,44), Quaternion.identity);
         platform.TravelSpeed = platformTravelSpeed;
+    }
+
+    private IEnumerator SpawnPlatforms() 
+    {
+        //if (platformsInWorld >= platformLimitCount) { yield return null; }
+        yield return new WaitForSeconds(18/ platformTravelSpeed - 0.2f);
+        Platform platform = Instantiate(basicPlatform[0], new Vector3(0, 0, 63), Quaternion.identity);
+        platform.TravelSpeed = platformTravelSpeed;
+        platform.transform.SetParent(platformsContainer);
+        StartCoroutine(SpawnPlatforms());
     }
 
 }
